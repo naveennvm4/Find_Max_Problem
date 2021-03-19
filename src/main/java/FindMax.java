12037a226070da0c0fdc4de52a060281451a64df
naveennvm4 <naveennvm4@gmail.com>
2021-03-19 09:30:19 +0530

    [Naveen]Added:Find Maximum in 3 String values

--- src/main/java/FindMax.java
+++ src/main/java/FindMax.java
@@ -10,5 +10,6 @@ public class FindMax {
     public static void main(String args[]) {
         System.out.printf("Maximum of %d, %d and %d is %d\n\n", 1, 2, 3, maximum(1, 2, 3));
         System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6, 8.8, 7.7));
+        System.out.printf("Maximum of %s, %s and %s is %s\n", "mango", "apple", "orange", maximum("mango", "apple", "orange"));
     }
 }
